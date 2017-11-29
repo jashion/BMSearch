d3d0b979d843ad5ed1a58ba9733118c657441972
Jashion <ycyz138258886@qq.com>
2017-11-29 10:37:17 +0800

    BMSearch project init.

---
+++ BMSearch/BMSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BMSearch
+//
+//  Created by Jashion on 2017/11/29.
+//  Copyright © 2017年 BMu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BMSearch/BMSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BMSearch
+//
+//  Created by Jashion on 2017/11/29.
+//  Copyright © 2017年 BMu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
